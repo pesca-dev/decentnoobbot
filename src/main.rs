1f48861ebfdf60f9cf0036e8fac2bbdc70ec7dd4
H1ghBre4k3r <h1ghbre4k3r@dev.bre4k3r.de>
2023-10-14 11:05:36 +0200

    feat(common): basic project setup

---
+++ src/main.rs
@@ -0,0 +1,28 @@
+use async_trait::async_trait;
+use disruption::{channel::Message, Client, Handler};
+use std::env;
+use std::error::Error;
+
+struct MyHandler;
+
+#[async_trait]
+impl Handler for MyHandler {
+    async fn on_message(&mut self, message: Message) {
+        match message.content() {
+            "!ping" => message.reply("Pong!").await.unwrap(),
+            "!echo" => message.reply("ECHO").await.unwrap(),
+            _ => {}
+        }
+    }
+}
+
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn Error>> {
+    env_logger::init();
+
+    let mut handler = MyHandler;
+
+    let mut client = Client::new(&mut handler, env::var("BOT_TOKEN")?);
+    client.connect().await?;
+    client.start().await
+}
